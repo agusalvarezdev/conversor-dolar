bbdae56ebca12e50ea326aa2e34bdcb4d4e5ddcc
Agus Alvarez <agusalvarez.dev@gmail.com>
2026-03-12 22:31:10 -0300

    actualizar conversor

--- conversor.py
+++ conversor.py
@@ -3,6 +3,6 @@ url = "https://dolarapi.com/v1/dolares/blue"
 respuesta =requests.get(url)
 datos = respuesta.json()
 precio_dolar= datos ["venta"]
-pesos=input("cuantos pesos tenes guardados ahora?")
+pesos=input("Ingresa el monto en pesos:")
 resultado= float(pesos) / precio_dolar
 print(f"Tenes {resultado:.2f} dolares")
